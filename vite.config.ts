9737e511fb3cd4423499fa9ff33a93ee2dda422e
Iryshka <iryna292@gmail.com>
2025-02-06 20:47:01 +0200

    Added routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true })],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
 });
